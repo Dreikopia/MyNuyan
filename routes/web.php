ef03e5daab3f21307cc8a14331f5404f7af712bc
Dreikopia <andreiheatherquintos@gmail.com>
2026-07-26 14:24:59 +0800

    Add various tooling

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('resident.home');
-});
+Route::get('/', fn() => view('resident.home'));
